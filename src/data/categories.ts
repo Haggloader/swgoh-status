c449552ac96a4e7a6b94be1f846213645f635f0e
Haggloader <danski.dansken@gmail.com>
2023-01-31 20:50:11 +0100

    Changed categories and added fleets.

--- src/data/categories.ts
+++ src/data/categories.ts
@@ -1,12 +1,14 @@
 import { flatten, forEach, map } from 'lodash'
 
 import events from './events'
+import fleets from './fleets'
 import legends from './galactic-legends'
 import { FarmCategory } from '../things/farm-category'
 
 export const categories = [
     legends,
     events,
+	fleets,
 ]
 
 export const flat_categories = [
