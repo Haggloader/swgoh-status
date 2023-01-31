4d86c2deaf813ab5dad9daea0c821f824f177de7
Haggloader <danski.dansken@gmail.com>
2023-01-31 20:38:33 +0100

    Changed API proxy

--- src/utils/fetch-json.ts
+++ src/utils/fetch-json.ts
@@ -1,5 +1,5 @@
 export default async function fetch_json(request: string): Promise<string> {
-    request = `https://cors-anywhere.freddie.wtf/${request}`
+    request = `https://corsproxy.io/?${encodeURIComponent(request)}`
     return fetch(request)
         .then(response => {
             if (response.ok) {
