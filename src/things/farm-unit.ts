5334c089205d2646aab72bee95c47546b1c9e249
Haggloader <danski.dansken@gmail.com>
2024-03-20 22:18:50 +0100

    test2speedunit

--- src/things/farm-unit.ts
+++ src/things/farm-unit.ts
@@ -3,12 +3,15 @@ export class FarmUnit {
     stars: number
     gear_level?: number
     relic_level?: number
+    speed: number
 
-    constructor(name: string, stars: number, gear_level?: number, relic_level?: number) {
+    constructor(name: string, stars: number, gear_level?: number, relic_level?: number, speed: number)
+    {
         this.name = name
         this.stars = stars
         this.gear_level = gear_level
         this.relic_level = relic_level
+        this.speed = speed
     }
 }
 
